4acd964477b75c964e5dfde16f3d5a97b73d70f7
omarqe <omar.veyron@gmail.com>
2017-06-27 18:18:35 +0800

    Update navigation bug when scrolling on mobile browser.

--- themes/briev/js/assets.js
+++ themes/briev/js/assets.js
@@ -10,21 +10,19 @@
 			$(window).on('resize', function(){
 				var t = $(this), s = $('.sidebar');
 
-				if ( t.width() > 768 ){
+				if ( t.width() > 768 )
 					s.css('width', '250px').children().show();
-				} else {
-					s.css('width', '0').children().hide();
-				}
 			});
 		},
 
 		toggleMenu: function(e){
 			e.preventDefault();
 
-			var t = $(this), s = $('.sidebar'), o = $('.sidebar-overlay');
+			var t = $(this), s = $('.sidebar'), o = $('.sidebar-overlay'), b = $('body, .global');
 
 			t.toggleClass('open');
 			if ( t.hasClass('open') ){
+				b.addClass('no-overflow');
 				o.show();
 				s.animate({
 					width:'250px'
@@ -33,6 +31,7 @@
 					s.children('.sidebar-top, .menu-content').fadeIn(300);
 				});
 			} else {
+				b.removeClass('no-overflow');
 				s.children('.sidebar-top, .menu-content').fadeOut(300, function(){
 					s.animate({width:0}, 200);
 					o.hide();
